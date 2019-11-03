cf73d23eced760b869b8c8a9dae1548cc9fe6fbc
wpzheng <569359120@qq.com>
2019-11-03 15:17:58 +0800

    update

--- module/route.js
+++ module/route.js
@@ -1,14 +1,34 @@
 var optfile = require('../libs/optfile')
+// var url = require('url')
+var querystring = require('querystring')
+
 
 // 根据pathname值 找到对应的处理函数
 module.exports = {
   login: function(req, res) {
+    // get方式
+    // var rdata = url.parse(req.url, true).query
+    // if(rdata['email']) {
+    //   console.log('参数',rdata)
+    // }
+
+    // post方式
+    var post = ''
+    req.on('data', function(chunk) {
+      post += chunk
+    })
+    req.on('end', function(){
+      post = querystring.parse(post)
+      console.log('post参数', post)
+    })
+    
+
     res.writeHead(200, {'Content-Type': 'text/html'})
     function recall(data) {
       res.write(data)
       res.end()
     }
-    optfile.readfile('../views/login.html', recall)
+    optfile.readfile('./views/login.html', recall)
   },
   demo: function(req, res) {
     res.write('我是DEMO')
