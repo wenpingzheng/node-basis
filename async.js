330529e83901d26d565269333ee8d24bc970ddb1
wpzheng <569359120@qq.com>
2019-11-10 16:09:38 +0800

    all

--- async.js
+++ async.js
@@ -1,10 +1,67 @@
 
 var async = require('async')
-async.series({
-  one: function(callback) {
+// 串行无关联
+// async.series({
+//   one: function(callback) {
+//     i = 0
+//     setInterval(function(){
+//       console.log('one' + new Date())
+//       i++
+//       if(i == 3) {
+//         clearInterval(this)
+//         callback(null, 'one完毕')
+//       }
+//     }, 1000);
+//   },
+//   two: function(callback) {
+//     j = 0
+//     setInterval(function(){
+//       console.log('two' + new Date())
+//       j++
+//       if(j == 3) {
+//         clearInterval(this)
+//         callback(null, 'two完毕')
+//       }
+//     }, 1000);
+//   }
+// },function(err, result) {
+//   console.log('result',result)
+// })
+
+// 并行无关联
+// async.parallel({
+//   one: function(callback) {
+//     i = 0
+//     setInterval(function(){
+//       console.log('one' + new Date())
+//       i++
+//       if(i == 3) {
+//         clearInterval(this)
+//         callback('error', 'one完毕')
+//       }
+//     }, 1000);
+//   },
+//   two: function(callback) {
+//     j = 0
+//     setInterval(function(){
+//       console.log('two' + new Date())
+//       j++
+//       if(j == 3) {
+//         clearInterval(this)
+//         callback(null, 'two完毕')
+//       }
+//     }, 1000);
+//   }
+// },function(err, result) {
+//   console.log('result',result)
+// })
+
+// 串行有关联
+async.waterfall([
+  function(callback) {
     i = 0
     setInterval(function(){
-      console.log('one' + new Date())
+      console.log('one' + i)
       i++
       if(i == 3) {
         clearInterval(this)
@@ -12,17 +69,17 @@ async.series({
       }
     }, 1000);
   },
-  two: function(callback) {
+  function(prevalue, callback) {
     j = 0
     setInterval(function(){
-      console.log('two' + new Date())
+      console.log(prevalue + ',two' + j)
       j++
       if(j == 3) {
         clearInterval(this)
-        callback(null, 'two完毕')
+        callback(null, prevalue + ',two完毕')
       }
     }, 1000);
   }
-},function(err, result) {
+],function(err, result) {
   console.log('result',result)
 })
